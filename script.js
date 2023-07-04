177a44071a6e19715ddb292adc4110ab7c44c8b3
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-07-04 13:35:31 +0300

    fixed high score bug

--- script.js
+++ script.js
@@ -60,7 +60,9 @@ function gameOver(){
     setTimeout(function(){
         $('body').css("background-color", "#011F3F");
     }, 200);
-    $('.score').text(currHighScore-1);
+    if (currHighScore > 0){
+        $('.score').text(currHighScore-1);
+    }
     
 }
 
@@ -90,94 +92,6 @@ function animatePress(userChosenColor){
         $("#"+userChosenColor).removeClass("clicked");
     }, 100);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function selectRandomTile(){
-//     let tileNum = Math.ceil(Math.random()*4);
-//     return tileNum;
-// }
-
-// function tileClassFromNumber(tileNum){
-//     if (tileNum == 1) return "green";
-//     else if (tileNum == 2) return "red";
-//     else if(tileNum == 3) return "yellow";
-//     else return "blue";
-// }
-
-// function playSound(tileClassName){
-//     $("."+tileClassName).addClass("clicked");
-//     let audio = new Audio("./sounds/"+tileClassName+".mp3");
-//     audio.play();
-//     setTimeout(function(){$("."+tileClassName).removeClass("clicked");}, 100);
-// }
-
-
-// function wrong(){
-//     $("body").addClass("wrong");
-//     setTimeout(function(){
-//         $("body").removeClass("wrong");
-//     }, 100);
-//     $("h1").text("Game Over, Press Any Key to Restart")
-// }
-
-// function checkClicked(){
-
-// }
-
-
-// function animateTile(tileNum){
-//     let tileClassName = tileClassFromNumber(tileNum);
-//     let audio = new Audio("./sounds/"+tileClassName+".mp3");
-//     audio.play();
-//     $("."+tileClassName).animate({
-//         opacity: 0.3
-//     }, 200).animate({ //the 150 is the duration of the transition
-//         opacity: 1
-//     }, 200)
-//     // if ( != tileNum){
-//     //     $("body").addClass("wrong");
-//     // }
-    
-// }
-
-// $(".tile").click(function() {
-//     alert(this.id);
-// });
-
-// $(document).keypress(function(){
-//     let flag = 0;
-//     let arrOfTiles = []; let keepTileTrack = 0;
-//     let userTiles = []; let userTileTrack = 0; 
-//     let initialTileNumber;
-//     if (flag === 0){
-//         initialTileNumber = selectRandomTile();
-//         flag = 1;
-//         $("h1").text("Level 1");
-//         animateTile(initialTileNumber);
-//         arrOfTiles.push(tileClassFromNumber(initialTileNumber));
-//     }
-//     while (userTiles.length < arrOfTiles.length){
-//         if (this.id != arrOfTiles[i]){
-//             wrong();
-//         }
-//     }
-
-
-
-// })
-
-
 
 
 
